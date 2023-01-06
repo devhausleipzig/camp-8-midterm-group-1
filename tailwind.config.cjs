259f1fcca3e7f591185f268dbe079c01ad057e60
ArthiRagothaman <arthi@example.com>
2023-01-06 14:25:27 +0100

    added google fonts and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+        dark:'#1C1C27',
+        white:'#FFFFFF',
+        whiteDimmed:'#FFFFFF66',
+        darkLight:'#363740',
+        yellow:'#FFB43A',
+        red:'#EF4444',
+        green:'#22C55E',
+        black:'#000000',
+
+    },
+    extend: {
+      fontFamily:{
+        Inter:['Inter', "sans-serif"],
+      }
+    },
   },
   plugins: [require("@tailwindcss/line-clamp")],
 };
