868f6e3d29a4f961e7cacc3c62f9ff19c80aeb85
ArthiRagothaman <arthi@example.com>
2023-01-09 15:24:00 +0100

    updated opacity value in tailwindconfig

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,20 +3,22 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      dark:'#1C1C27',
-      white:'#FFFFFF',
-      darkLight:'#363740',
-      yellow:'#FFB43A',
-      red:'#EF4444',
-      green:'#22C55E',
-      black:'#000000',
-      background:'#353333',
-
-  },
+      dark: "#1C1C27",
+      white: "#FFFFFF",
+      darkLight: "#363740",
+      yellow: "#FFB43A",
+      red: "#EF4444",
+      green: "#22C55E",
+      black: "#000000",
+      background: "#353333",
+    },
     extend: {
-      fontFamily:{
-        Inter:['Inter', "sans-serif"],
-      }
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      },
+      opacity: {
+        40: ".4",
+      },
     },
   },
   plugins: [require("@tailwindcss/line-clamp")],
