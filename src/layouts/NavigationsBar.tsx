976fafb9d73f95ab0e4e17f8eef7b2b61a8d942f
ArthiRagothaman <arthi@example.com>
2023-01-10 16:15:01 +0100

    updated as an array object

--- src/layouts/NavigationsBar.tsx
+++ src/layouts/NavigationsBar.tsx
@@ -2,24 +2,39 @@ import { HomeIcon } from "@heroicons/react/24/solid";
 import { FilmIcon } from "@heroicons/react/24/solid";
 import { UserIcon } from "@heroicons/react/24/solid";
 import { Bars4Icon } from "@heroicons/react/24/solid";
-import { NavLink } from "react-router-dom";
 import clsx from "clsx";
 import { useState } from "react";
 
 export default function Navigationsbar() {
   const [selected, setSelected] = useState(0);
+  const icons = [
+    {
+      name: <HomeIcon/>
+    },
+    {
+      name: <FilmIcon />
+    },
+    {
+      name: <UserIcon />
+    },
+    {
+      name: <Bars4Icon />
+    },
+  ];
 
   return (
     <div className=" w-[375px] h-[88px] bg-dark ">
       <nav className="flex items-center justify-center mt-5 mx-3 ">
-        <HomeIcon
+        {icons.map((icon) => ({icon.name}))}
+
+        {/* <HomeIcon
           className={clsx(
             " w-16 h-16 mx-[2rem] ",
             selected == 0 ? "text-white" : "text-white-dimmed"
           )}
           onClick={() => setSelected(0)}
-        />
-        {/*className= {({ isActive}) => clsx("hover: text-white", isActive && "text-white w-16 h-16")} />  */}
+        /> 
+
         <FilmIcon
           className={clsx(
             " w-16 h-16 mx-[2rem]",
@@ -40,52 +55,7 @@ export default function Navigationsbar() {
             selected == 2 ? "text-white" : "text-white-dimmed"
           )}
           onClick={() => setSelected(2)}
-        />
-
-        {/* <NavLink
-          className={({ isActive }) =>
-            clsx(
-              "hover: text-white",
-              isActive && " text-white-dimmed w-16 h-16"
-            )
-          }
-          to={""}
-        >
-          <HomeIcon></HomeIcon>
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            clsx(
-              "hover: text-white",
-              isActive && " text-white-dimmed w-16 h-16"
-            )
-          }
-          to={""}
-        >
-          <FilmIcon></FilmIcon>
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            clsx(
-              "hover: text-white",
-              isActive && " text-white-dimmed w-16 h-16"
-            )
-          }
-          to={""}
-        >
-          <UserIcon></UserIcon>
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            clsx(
-              "hover: text-white",
-              isActive && " text-white-dimmed w-16 h-16"
-            )
-          }
-          to={""}
-        >
-          <Bars4Icon></Bars4Icon>
-        </NavLink> */}
+        /> */}
       </nav>
     </div>
   );
