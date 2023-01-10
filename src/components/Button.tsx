13f35acfa361fa31ef8d31b1af45d996287ece90
Regina W <whyder.bagina@gmail.com>
2023-01-10 14:28:17 +0100

    1 of 3 button states

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -24,26 +24,11 @@ type Input = {
   type: string;
 };
 export function Button({ type }: Input) {
-  return (
-    <div className="flex items-center">
-      <button className="rounded-md border-2 border-solid shadow-sm px-4 py-2 font-medium text-slate-700">
-        ButtonText
-      </button>
-
-      {/* <button
-        className="rounded-md border-2 border-transparent shadow-sm px-4 py-2 bg-yellow-400 opacity-40
-            text-base font-medium text-slate-700"
-        data-ripple-light="true"
-      >
-        ButtonActive
-      </button>
-      <button
-        className="rounded-md border-2 border-transparent shadow-sm px-4 py-2 bg-slate-700
-            text-base font-medium text-white"
-        data-ripple-dark="true"
-      >
-        ButtonDisabled
-      </button> */}
-    </div>
-  );
+  if ((type = "Normal")) {
+    return (
+      <div className="bg-background">
+        <button className="text-title">ButtonText</button>
+      </div>
+    );
+  }
 }
