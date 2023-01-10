65b0557fabed15b259840ac53c568729b077ccf4
William de Vial <william.d.devial@gmail.com>
2023-01-10 14:30:29 +0100

    Added UseState

--- src/layouts/NavigationsBar.tsx
+++ src/layouts/NavigationsBar.tsx
@@ -4,14 +4,26 @@ import { UserIcon } from "@heroicons/react/24/solid";
 import { Bars4Icon } from "@heroicons/react/24/solid";
 import { NavLink } from "react-router-dom";
 import clsx from "clsx";
+import { useState } from "react";
 
 export default function Navigationsbar() {
+  const [selected, setSelected] = useState(0);
+
   return (
     <div className=" w-[375px] h-[88px] bg-dark ">
       <nav className="flex items-center justify-center mt-5 mx-3 ">
-        <HomeIcon className=" text-white w-16 h-16 mx-[2rem] " />{" "}
+        <HomeIcon
+          className={clsx(
+            " w-16 h-16 mx-[2rem] ",
+            selected == 0 ? "text-white" : "text-white-dimmed"
+          )}
+          onClick={() => setSelected(0)}
+        />
         {/*className= {({ isActive}) => clsx("hover: text-white", isActive && "text-white w-16 h-16")} />  */}
-        <FilmIcon className=" text-white-dimmed w-16 h-16 mx-[2rem]" />
+        <FilmIcon
+          className=" text-white-dimmed w-16 h-16 mx-[2rem]"
+          onClick={() => setSelected(1)}
+        />
         <UserIcon className=" text-white-dimmed w-16 h-16 mx-[2rem]" />
         <Bars4Icon className=" text-white-dimmed w-16 h-16 mx-[2rem]" />
         {/* <NavLink
