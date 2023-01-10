9dc4f8c95fdea698e02bd5edb2bc42861b17554a
Regina W <whyder.bagina@gmail.com>
2023-01-10 16:10:14 +0100

    button styling hard coded

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,26 +4,26 @@ import React, { useState } from "react";
 type ButtonProps = {
   primary: boolean;
   backgroundColor: string;
-  label: string;
   size: "large";
 };
 
 type Input = {
   type: string;
+  label: string;
 };
 export function Button({ type }: Input) {
   return (
-    <div className="bg-background">
+    <div className="flex justify-center items-center">
       <button
         type="button"
         className={clsx(
-          "text-center rounded-lg w-full text-primary",
+          "text-center text-primary rounded-lg w-[335px] h-[49px] ",
           type === "Normal" ? "bg-yellow text-dark-light" : "",
           type === "Active" ? "bg-yellow bg-opacity-50 text-dark-light" : "",
           type === "Disabled" ? "bg-dark-light text-white" : ""
         )}
       >
-        text
+        {}
       </button>
     </div>
   );
