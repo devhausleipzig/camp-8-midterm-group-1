bd64528231992e5fdf475c140208eecb56ed69dc
ArthiRagothaman <arthi@example.com>
2023-01-10 13:24:11 +0100

    added heroicons nd clsx

---
+++ src/layouts/NavigationsBar.tsx
@@ -0,0 +1,24 @@
+import { HomeIcon } from "@heroicons/react/24/solid";
+import { FilmIcon } from "@heroicons/react/24/solid";
+import { UserIcon } from "@heroicons/react/24/solid";
+import { Bars4Icon } from "@heroicons/react/24/solid";
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
+
+export default function Navigationsbar() {
+  return (
+    <div className=" w-[375px] h-[88px] bg-[#1C1C27] ">
+      <nav className="flex items-center justify-center mt-5 mx-3 ">
+        {
+          <HomeIcon className="text-white w-16 h-16 mx-[2rem] " /> /*className= {({ isActive}) => clsx("hover: text-white", isActive && "text-white w-16 h-16")} /> */
+        }
+
+        <FilmIcon className=" text-white opacity-40 w-16 h-16 mx-[2rem]" />
+        <UserIcon className=" text-white opacity-40 w-16 h-16 mx-[2rem]" />
+        <Bars4Icon className=" text-white opacity-40 w-16 h-16 mx-[2rem]" />
+
+        
+      </nav>
+    </div>
+  );
+}
