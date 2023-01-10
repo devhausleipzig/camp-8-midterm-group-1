57f8903357771a73755ba54e1c59d647ab6ca53b
n1lsw <n1ls@pop-os.localdomain>
2023-01-10 12:01:03 +0100

    dummy home page - I will add the components as soon as their are confirmed

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,22 @@
 import { NavigationBar } from "../components/NavigationBar";
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <NavigationBar />
+    <div className="bg-slate-800 w-[335px] h-screen grid grid-rows-6">
+      <div className="w-full h-24 bg-slate-800 text-white text-2xl flex justify-center items-center">
+        UserInfo
+      </div>
+      <div className="w-full h-24 bg-slate-800 text-white text-2xl flex justify-center items-center">
+        SearchInput
+      </div>
+      <div className="w-full h-24 bg-slate-800 text-white text-2xl flex justify-center items-center">
+        Icon buttons
+      </div>
+      <div className="w-full h-24 bg-slate-800 text-white text-2xl flex justify-center items-center">
+        UpComing movies
+      </div>
+      <div className="grid row-start-6 content-center">
+        <NavigationBar />
+      </div>
     </div>
   );
 }
