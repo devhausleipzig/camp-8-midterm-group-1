ac667cfdb5f1bf47d8d5a93cdaea7f571693acb7
ArthiRagothaman <arthi@example.com>
2023-01-10 14:16:42 +0100

    commentted Navlink in Navpage

--- src/layouts/NavigationsBar.tsx
+++ src/layouts/NavigationsBar.tsx
@@ -7,17 +7,57 @@ import clsx from "clsx";
 
 export default function Navigationsbar() {
   return (
-    <div className=" w-[375px] h-[88px] bg-[#1C1C27] ">
+    <div className=" w-[375px] h-[88px] bg-dark ">
       <nav className="flex items-center justify-center mt-5 mx-3 ">
-        {
-          <HomeIcon className="text-white w-16 h-16 mx-[2rem] " /> /*className= {({ isActive}) => clsx("hover: text-white", isActive && "text-white w-16 h-16")} /> */
-        }
-
-        <FilmIcon className=" text-white opacity-40 w-16 h-16 mx-[2rem]" />
-        <UserIcon className=" text-white opacity-40 w-16 h-16 mx-[2rem]" />
-        <Bars4Icon className=" text-white opacity-40 w-16 h-16 mx-[2rem]" />
-
-        
+        <HomeIcon className=" text-white w-16 h-16 mx-[2rem] " />{" "}
+        {/*className= {({ isActive}) => clsx("hover: text-white", isActive && "text-white w-16 h-16")} />  */}
+        <FilmIcon className=" text-white-dimmed w-16 h-16 mx-[2rem]" />
+        <UserIcon className=" text-white-dimmed w-16 h-16 mx-[2rem]" />
+        <Bars4Icon className=" text-white-dimmed w-16 h-16 mx-[2rem]" />
+        {/* <NavLink
+          className={({ isActive }) =>
+            clsx(
+              "hover: text-white",
+              isActive && " text-white-dimmed w-16 h-16"
+            )
+          }
+          to={""}
+        >
+          <HomeIcon></HomeIcon>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            clsx(
+              "hover: text-white",
+              isActive && " text-white-dimmed w-16 h-16"
+            )
+          }
+          to={""}
+        >
+          <FilmIcon></FilmIcon>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            clsx(
+              "hover: text-white",
+              isActive && " text-white-dimmed w-16 h-16"
+            )
+          }
+          to={""}
+        >
+          <UserIcon></UserIcon>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            clsx(
+              "hover: text-white",
+              isActive && " text-white-dimmed w-16 h-16"
+            )
+          }
+          to={""}
+        >
+          <Bars4Icon></Bars4Icon>
+        </NavLink> */}
       </nav>
     </div>
   );
