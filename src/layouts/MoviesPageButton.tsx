560cf82fc73ff601fcbf987fab12a5120336dabc
ArthiRagothaman <arthi@example.com>
2023-01-11 11:49:47 +0100

    added function

--- src/layouts/MoviesPageButton.tsx
+++ src/layouts/MoviesPageButton.tsx
@@ -3,30 +3,29 @@ import { useState } from "react";
 
 export default function Moviespagebutton() {
   const [button, setButton] = useState(0);
-  return (
-    <>
+  function Movies(Number: Number) {
+    return (
       <div className="w-[72px] h-[124px] flex flex-col items-center justify-center gap-[1.25rem] bg-dark">
         <button
           className={clsx(
             "w-[32px] h-[32px] rounded-sm text-dark",
 
-            button == 0 ? " bg-yellow" : " bg-white-dimmed"
+            Number == 0 ? " bg-yellow" : " bg-white-dimmed"
           )}
           onClick={() => setButton(0)}
         >
           1
         </button>
-        <button
-          className={clsx(
-            "w-[32px] h-[32px] rounded-sm text-dark",
-
-            button == 1 ? " bg-yellow" : " bg-white-dimmed"
-          )}
-          onClick={() => setButton(1)}
-        >
-          1
-        </button>
       </div>
+    );
+  }
+  return (
+    <>
+      {Movies(1)}
+      {Movies(2)}
+      {Movies(3)}
+      {Movies(4)}
+      {Movies(5)}
     </>
   );
 }
