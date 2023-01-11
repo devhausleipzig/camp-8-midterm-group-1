0b56830056a50ca93dc00f968c709f3a3675dba4
n1lsw <n1ls@pop-os.localdomain>
2023-01-11 09:49:49 +0100

    test

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,66 +3,89 @@ import { redirect, useNavigate } from "react-router-dom";
 import { Button } from "./Button";
 
 export function Input() {
-  const navigate = useNavigate();
-  const submitfunction = async (event) => {
-    event.preventDefault();
+  // const navigate = useNavigate();
+  // const submitfunction = async (event) => {
+  //   event.preventDefault();
 
-    // const formData = new FormData(form);
-    // const fromEntries = formData.entries();
-    // const data = Object.fromEntries(formData.entries()) as Record<
-    //   string,
-    //   string | string[]
-    // >;
-
-    navigate("/");
-
-    // const user: Record<string, any> = await axios.post(
-    //   "http://0.0.0.0:3000/users",
-    //   data
-    // );
-    // console.log(user.data);
-
-    // alert(`Successfully registered! Your user id is: ${user.data.id}`);
-  };
+  //   navigate("/");
+  // };
 
   return (
-    <div className="w-screen h-screen flex justify-center">
-      <form
-        id="form-placeholder"
-        className="w-[375px] h-[667px] bg-[#1C1C27] flex flex-col gap-5 rounded-md"
-        onSubmit={submitfunction}
-      >
-        <div className="flex flex-col">
-          <div className="gap-5 text-slate-300 justify-center grid grid-rows-[1fr_1fr_1fr_8fr_1fr]">
-            <div className="flex items-end">Welcome to Cine-Scape</div>
-            <div className="w-[335px]">
-              You need to log in to be able to make revervations and add movies
-              to your watchlist
-            </div>
-            {/* put here the button/login components created by Regina */}
-            <div>
-              <input
-                type="email"
-                name="email-address"
-                placeholder="Your@email.com"
-                className="rounded-md bg-[#363740] h-[48px] w-[335px]"
-              />
-            </div>
-            <div>
-              <input
-                type="password"
-                name="password"
-                placeholder="Enter your password"
-                className="rounded-md bg-[#363740] h-[48px] w-[335px]"
-              />
-            </div>
-            <div className="flex items-end">
-              {/* Dummy button. Use the ones by Regina */}
-              <Button></Button>
-            </div>
-          </div>
+    <form
+      id="form-placeholder"
+      className="w-96 h-[667px] bg-dark flex flex-col rounded-md"
+      // onSubmit={submitfunction}
+    >
+      <div className="flex flex-col gap-5">
+        <div className="flex items-end text-title">Welcome to Cine-Scape</div>
+        <div className="w-80 text-body">
+          You need to log in to be able to make revervations and add movies to
+          your watchlist
         </div>
-      </form>
-    </div>
+        {/* put here the button/login components created by Regina */}
+        <div className="relative">
+          <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+            <button
+              type="submit"
+              className="px-4 focus:outline-none focus:shadow-outline text-body"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                />
+              </svg>
+            </button>
+          </span>
+          <input
+            type="email"
+            name="email-address"
+            placeholder="Your@email.com"
+            className="rounded-md bg-dark-light text-body h-12 w-80 hover:border-2 hover:border-white-dimmed-heavy  focus:outline-none focus:text-red-500 pl-16"
+          />
+        </div>
+        <div className="relative">
+          <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+            <button
+              type="submit"
+              className="px-4 focus:outline-none focus:shadow-outline text-body"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"
+                />
+              </svg>
+            </button>
+          </span>
+          <input
+            type="email"
+            name="email-address"
+            placeholder="Enter your password"
+            className="rounded-md bg-dark-light text-body h-12 w-80 hover:border-2 hover:border-white-dimmed-heavy  focus:outline-none focus:text-red-500 pl-16"
+          />
+        </div>
+        <div className="flex items-end">
+          {/* Dummy button. Use components later */}
+          {/* <Button></Button> */}
+        </div>
+      </div>
+    </form>
   );
 }
--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import { Input } from "../components/Input";
-
-export function Login() {
-  return <Input></Input>;
-=======
 export function Login() {
   return (
     <div>
       <h1>Login</h1>
     </div>
   );
->>>>>>> main
 }
