deedbda7270b29075ec0afe5a4821ee24fc897b7
William de Vial <william.d.devial@gmail.com>
2023-01-11 11:58:50 +0100

    fixed

--- src/layouts/MoviesPageButton.tsx
+++ src/layouts/MoviesPageButton.tsx
@@ -2,30 +2,35 @@ import clsx from "clsx";
 import { useState } from "react";
 
 export default function Moviespagebutton() {
-  const [button, setButton] = useState(0);
-  function Movies(Number: Number) {
+  const [button, setButton] = useState<number>(0);
+  function Movies(Number: number) {
     return (
       <div className="w-[72px] h-[124px] flex flex-col items-center justify-center gap-[1.25rem] bg-dark">
         <button
           className={clsx(
             "w-[32px] h-[32px] rounded-sm text-dark",
 
-            Number == 0 ? " bg-yellow" : " bg-white-dimmed"
+            button == Number ? " bg-yellow" : " bg-white-dimmed"
           )}
-          onClick={() => setButton(0)}
+          onClick={() => {
+            setButton(Number);
+            console.log("clicked", Number, button);
+          }}
         >
-          1
+          {String(Number + 1)}
         </button>
       </div>
     );
   }
   return (
     <>
-      {Movies(1)}
-      {Movies(2)}
-      {Movies(3)}
-      {Movies(4)}
-      {Movies(5)}
+      <div className="flex gap-4">
+        {Movies(0)}
+        {Movies(1)}
+        {Movies(2)}
+        {Movies(3)}
+        {Movies(4)}
+      </div>
     </>
   );
 }
