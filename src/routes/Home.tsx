5b97b069a0d6aa90c0e31368429cafda84702127
Marhamat <M.berhan@gmx.de>
2023-01-12 16:12:36 +0100

    BookingLabel and SeatSquares

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,6 @@
 import { NavigationBar } from "../components/NavigationBar";
+import { BookingLabel } from "../components/BookingLabel";
+import { SeatSquares } from "../components/SeatSquares";
 export function Home() {
   return (
     <div>
