c4f9eadc8c2f02bb55007b2b2a0f28b1c9ca16ca
William de Vial <william.d.devial@gmail.com>
2023-01-12 09:41:30 +0100

    Simple Emogi Component

---
+++ src/stories/Emogi.stories.tsx
@@ -0,0 +1,19 @@
+import { Emogi } from "../components/Emogi";
+import { Meta, StoryFn } from "@storybook/react";
+import "../index.css";
+
+type NewType = Meta<typeof Emogi>;
+
+export default {
+  title: "Example/Emogi",
+  component: Emogi,
+  argTypes: {},
+} as NewType;
+
+const Template: StoryFn<typeof Emogi> = (args) => <Emogi {...args} />;
+
+export const Generic = Template.bind({});
+
+Generic.args = {
+  emojiName: "Adventure",
+};
