99cd379daf348c63e5603196a2be5b5f109b9cc0
William de Vial <william.d.devial@gmail.com>
2023-01-12 09:30:24 +0100

    Sinplified. Data will be loaded on Home element when created

--- src/components/Carroussel.tsx
+++ src/components/Carroussel.tsx
@@ -1,19 +1,18 @@
-import { useState } from "react";
-import { API, DataResponce } from "../components/API";
+import { Movie } from "../types/api";
 
-export function Carroussel() {
-  const data = new API();
-  const [Array, setArray] = useState<DataResponce[]>([]);
-  data.BuildArray(setArray);
+type Input = {
+  Array: Movie[];
+};
 
+export function Carroussel({ Array }: Input) {
   return (
-    <div className="flex flex-row gap-8 overflow-x-auto super-width">
+    <div className="flex flex-row gap-8 overflow-x-auto">
       {Array.map((film) => {
         return (
           <img
             src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
             alt=""
-            className="w-44 h-56 rounded-lg flex-grow-0"
+            className="w-44 h-56 rounded-lg flex-grow-0 object-cover"
             key={film.id}
           />
         );
