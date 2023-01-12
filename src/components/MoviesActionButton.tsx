65ac1590fa03b793b00ea0ceec5e5b41292d76d9
William de Vial <william.d.devial@gmail.com>
2023-01-12 15:55:18 +0100

    made scalable

--- src/components/MoviesActionButton.tsx
+++ src/components/MoviesActionButton.tsx
@@ -1,10 +1,15 @@
-export default function MoviesActionButton() {
+type Input = {
+  image: string;
+  name: string;
+  role: string;
+};
+export default function MoviesActionButton({ image, name, role }: Input) {
   return (
     <div className="flex items-center gap-[1.25rem]">
-      <div className="w-[64px] h-[64px] bg-white"></div>
+      <img src={image} alt="" />
       <div className="flex flex-col">
-        <p className=" text-white">Actor Name</p>
-        <p className=" text-white-dimmed text-description">Character</p>
+        <p className=" text-white">{name}</p>
+        <p className=" text-white-dimmed text-description">{role}</p>
       </div>
     </div>
   );
