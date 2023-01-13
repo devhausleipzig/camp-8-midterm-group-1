b0344cd0a087c363d23952dff3b8d14f16dea19d
William de Vial <william.d.devial@gmail.com>
2023-01-13 11:48:08 +0100

    preparing to merge Layout changes

--- src/routes/Cast.tsx
+++ src/routes/Cast.tsx
@@ -1,7 +1,18 @@
-export function CastCrew() {
+import { MoviesType } from "./Movies";
+import { LoaderFunctionArgs, useLoaderData } from "react-router-dom";
+import { API } from "../components/API";
+
+export async function movieLoader({ params }: LoaderFunctionArgs) {
+  const result = API.castAndCrew(params.)
+}
+
+export function Movie() {
+  const movie = useLoaderData() as MoviesType;
   return (
     <div>
-      <h1>Cast & Crew</h1>
+      <p>Hello world, I am a movie</p>
+      <h2>{movie.id}</h2>
+      <p className="bg-red">{movie.id}</p>
     </div>
   );
 }
