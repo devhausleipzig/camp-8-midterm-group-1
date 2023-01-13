a1cd7d050e231009f00cc410b96c6fac728acbf2
William de Vial <william.d.devial@gmail.com>
2023-01-13 16:16:45 +0100

    Added Maths

--- src/routes/Movie.tsx
+++ src/routes/Movie.tsx
@@ -1,6 +1,7 @@
 import { LoaderFunctionArgs, useLoaderData } from "react-router-dom";
 import { MovieDetail } from "../types/api";
 import { API } from "../components/API";
+import clsx from "clsx";
 
 export async function movieLoader({ params }: LoaderFunctionArgs) {
   const result = API.movieDetail(Number(params.movieId));
@@ -12,36 +13,45 @@ export function Movie() {
     const hours = Math.floor(minutes / 60);
     const Minutes = minutes % 60;
 
-    return `${hours}h + ${Minutes}m`;
+    return `${hours}h ${Minutes}m`;
   }
-
   const mov = useLoaderData() as MovieDetail;
+  const score = Math.floor(mov.vote_average * 10);
 
   return (
     <>
       <div className="flex flex-col h-screen pl-5 pr-5 ">
         <div className="flex flex-col gap-y-6">
           <img
-            className="w-80 h-52 bg-cover border rounded-lg "
+            className="w-80 h-52 border rounded-lg object-cover"
             src={"https://image.tmdb.org/t/p/w500" + mov.poster_path}
           />
           <p className="text-supertitle text-white pb-3">{mov.title}</p>
         </div>
 
-        <div className="flex items-start gap-6">
-          <p className="flex text-description text-white">
-            {mov.release_date.split("-")[0]}
-          </p>
-          <p className="text-description text-white-dimmed  ">
-            {mov.genres.map((x) => x.name).join("/")}
-          </p>
-          <p className="  text-description text-white-dimmed pb-3 ">
-            {mov.runtime}
-          </p>
-        </div>
-        <div className="flex ">
-          <p className="  test-description text-white-dimmed">
-            {mov.vote_average} Score
+        <div className="flex justify-between items-start">
+          <div className="flex items-start gap-6">
+            <p className="flex text-description text-white">
+              {mov.release_date.split("-")[0]}
+            </p>
+            <p className="text-description text-white-dimmed  ">
+              {mov.genres
+                .filter((_, index) => index < 2)
+                .map((x) => x.name)
+                .join("/")}
+            </p>
+            <p className="  text-description text-white-dimmed pb-3 ">
+              {String(minutesToHours(mov.runtime as number))}
+            </p>
+          </div>
+          <p
+            className={clsx(
+              "text-description",
+              score > 50 ? "text-green" : "text-red"
+            )}
+          >
+            {String(score) + "%"}{" "}
+            <span className="text-white-dimmed">Score</span>
           </p>
         </div>
 
