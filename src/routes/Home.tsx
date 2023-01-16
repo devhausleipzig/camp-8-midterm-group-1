b562313f291075002acedcbb8d41e03a055d6634
William de Vial <william.d.devial@gmail.com>
2023-01-16 11:49:35 +0100

    Removed tests from main

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,6 @@
 import { Days } from "../components/MockData";
 
 export function Home() {
-  const data = new Days();
-  console.log(data.days);
-  console.log(data.days.map((day) => day.isFull()));
-
   return (
     <div>
       <h1>Home</h1>
