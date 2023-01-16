bd51f73c8c1962176c66b291491ed6fa63319618
Marhamat <M.berhan@gmx.de>
2023-01-16 15:00:46 +0100

    SelectSeats added

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,7 @@
 import { NavigationBar } from "../components/NavigationBar";
 import { BookingLabel } from "../components/BookingLabel";
 import { SeatSquares } from "../components/SeatSquares";
+import { SelectSeats } from "../components/SelectSeats";
 export function Home() {
   return (
     <div>
