f190a213cd37a056b73deb6cb3ff6e0d572f705d
William de Vial <william.d.devial@gmail.com>
2023-01-16 12:58:11 +0100

    Added hidden scrollbar and snapping

--- src/components/Carroussel.tsx
+++ src/components/Carroussel.tsx
@@ -1,18 +1,18 @@
 import { Movie } from "../types/api";
 
-type Input = {
-  Array: Movie[];
+type InputProps = {
+  selection: Movie[];
 };
 
-export function Carroussel({ Array }: Input) {
+export function Carroussel({ selection }: InputProps) {
   return (
-    <div className="flex flex-row gap-8 overflow-x-auto">
-      {Array.map((film) => {
+    <div className="flex flex-row gap-8 overflow-x-auto snap-x scrollbar-hide">
+      {selection.map((film) => {
         return (
           <img
             src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
             alt=""
-            className="w-44 h-56 rounded-lg flex-grow-0 object-cover"
+            className="w-44 h-56 rounded-lg flex-grow-0 object-cover snap-center"
             key={film.id}
           />
         );
