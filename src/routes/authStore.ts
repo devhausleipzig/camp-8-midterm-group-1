c90d5ca7c7f16e88fa552f8bb54fd61e09faf027
peeler <t.delourme11@gmail.com>
2023-01-18 14:34:41 +0100

    final try

--- src/routes/authStore.ts
+++ src/routes/authStore.ts
@@ -1,38 +1,35 @@
 import create from "zustand";
 import { persist } from "zustand/middleware";
 import zustand from "zustand";
-import { UserCircleIcon } from "@heroicons/react/24/solid";
 
 type AuthStore = {
   user: User;
   token: string;
-  setUser: (inputUser: string) => void;
+  setUser: (inputUser: User) => void;
   setToken: (tokenResponse: string) => void;
   clearAuth: () => void;
 };
 
-type User =  {
+type User = {
   username: string;
   avatar: string;
-}
+};
 
 const initState = {
-    user: "",
-    token: "",
+  user: { username: "", avatar: "" },
+  token: "",
 };
 export const useAuthStore = create<AuthStore>()(
-    persist(
-      (set) => ({
-        user: {username: "", avatar:""},
-        token: "",
-        setUser: (inputUser: string) => set({ user: {...user, username: inputUser} }),
-        setToken: (tokenResponse: string) => set({ token: tokenResponse }),
-        clearAuth: () => set({ ...initState }),
-      }),
-      {
-        name: "blog-auth",
-      }
-    )
-  );
-  
-    
+  persist(
+    (set) => ({
+      user: { username: "", avatar: "" },
+      token: "",
+      setUser: (inputUser: User) => set({ user: inputUser }),
+      setToken: (tokenResponse: string) => set({ token: tokenResponse }),
+      clearAuth: () => set({ ...initState }),
+    }),
+    {
+      name: "page-auth",
+    }
+  )
+);
