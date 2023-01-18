049ce7c624e9b60abc3399f9a72351f264cdb49f
Marhamat <M.berhan@gmx.de>
2023-01-18 13:53:41 +0100

    added new things

---
+++ src/components/SeatsLocationPrice.tsx
@@ -0,0 +1,59 @@
+type SeatsLocationPriceProps = {
+  primary: boolean;
+  Location: string;
+  Price: number;
+};
+type Input = "Front" | "Middle" | "Back";
+const Object: Record<Input, number> = {
+  Front: 12.95,
+  Middle: 14,
+  Back: 16,
+};
+// export function SeatsLocationPrice({ [{Front:2}, {Middle:5}, {Back"6}] }: Input) {
+//   return (
+//     {Object.Front}
+//   );
+// }
+
+// const map1 = new Map<string, string | number>([
+//   ['name', 'Tom'],
+//   ['country', 'Germany'],
+//   ['age', 30],
+// ]);
+
+// ✅ Using forEach
+map1.forEach((value, key) => {
+  console.log(value, key); // 👉️ Tom name, Germany country, 30 age
+});
+
+/*
+    Higher order functions
+    - sort -> reorders elements in an array => reference to the mutated array
+    - map
+*/
+
+function map(
+  arr: number[],
+  callbackFn: (value: number, index: number, array: number[]) => number
+) {
+  let newArray: number[] = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    const result = callbackFn(element, i, arr);
+    newArray.push(result);
+  }
+
+  return newArray;
+}
+
+function double(value: number) {
+  return value * 2;
+}
+
+function square(value: number) {
+  return value * value;
+}
+
+console.log(map([1, 2, 3], double));
+console.log(map([1, 2, 3], square));
