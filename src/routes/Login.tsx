81fdf1a33158c10d77ddc94872a3026680e6ec25
n1lsw <nils.wilharm@mailbox.org>
2023-01-18 11:30:36 +0100

    Cleared Login route of unnecessary code

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -2,12 +2,8 @@ import { Input } from "../components/Input";
 
 export function Login() {
   return (
-    <form
-      id="form-placeholder"
-      className="w-full bg-dark flex flex-col rounded-md px-5 py-8 h-screen"
-    >
-      <Input></Input>
-      {/* {Button will go here} */}
-    </form>
+    <div>
+      <h1>Login</h1>
+    </div>
   );
 }
