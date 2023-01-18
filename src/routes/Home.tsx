ce9c62d60a6c09d9d2c5c5cc40ac3c264063cf7c
Marhamat <M.berhan@gmx.de>
2023-01-18 13:42:42 +0100

    deleted unnesecery files

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,4 @@
 import { NavigationBar } from "../components/NavigationBar";
-import { BookingLabel } from "../components/BookingLabel";
-import { SeatSquares } from "../components/SeatSquares";
 export function Home() {
   return (
     <div>
