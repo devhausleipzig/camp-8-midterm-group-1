368cbae02cd6f29be495d9818e76aa062ab862be
peeler <t.delourme11@gmail.com>
2023-01-19 10:57:51 +0100

    final version

--- src/routes/Account.tsx
+++ src/routes/Account.tsx
@@ -1,17 +1,11 @@
-import { Navigate, useNavigate } from "react-router-dom";
 import { Button, ButtonVariant } from "../components/Button";
 import { useAuthStore } from "./authStore";
 
-const navigate = useNavigate;
-
 export function Account() {
-  const { token, clearAuth } = useAuthStore();
-  if (!token) return <Navigate to="/login" replace />;
+  const { clearAuth } = useAuthStore();
 
   return (
-    <div className=" h-full ">
-      <h1>Login</h1>
-      <h1>Account</h1>
+    <div>
       <div className="flex justify-center">
         <Button
           variant={ButtonVariant.secondary}
