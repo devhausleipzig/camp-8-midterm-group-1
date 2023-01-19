35c646b1bec726ae7af051f46557a44d6b61055e
peeler <t.delourme11@gmail.com>
2023-01-19 11:41:47 +0100

    draft 2

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,7 +1,11 @@
 import { EnvelopeIcon, KeyIcon } from "@heroicons/react/24/solid";
+import { Navigate } from "react-router-dom";
 import { Input } from "../components/Input";
+import { useAuthStore } from "./authStore";
 
 export function Login() {
+  const { token } = useAuthStore();
+  if (!token) return <Navigate to="/home" replace />;
   return (
     <div>
       <h1>Login</h1>
