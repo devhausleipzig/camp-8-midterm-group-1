3a65c0c07c65994b283ad6756e4346dc4703fa67
Chirag Singal <chirag.prakriya@gmail.com>
2023-01-19 15:02:18 +0100

    removed absolute tailwind classes

--- src/components/ScreenComponent.tsx
+++ src/components/ScreenComponent.tsx
@@ -2,10 +2,10 @@ export function ScreenComponent() {
   return (
     <>
       <div className="flex items-center justify-center mt-6">
-        <hr className="w-[279px] h-1 bg-yellow" />
+        <hr className="w-4/5 h-1 bg-yellow" />
       </div>
       <div className="flex items-center justify-center">
-        <hr className="w-[279px] h-5 rounded bg-gradient-to-b from-yellow via-white-dimmed to-white-dimmed-heavy opacity-20" />
+        <hr className="w-4/5 h-5 rounded bg-gradient-to-b from-yellow via-white-dimmed to-white-dimmed-heavy opacity-20" />
       </div>
     </>
   );
