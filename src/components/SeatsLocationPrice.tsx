9290501ce314b599ee343f53b31acd511520d94d
Marhamat <M.berhan@gmx.de>
2023-01-23 14:01:54 +0100

    select seats edited

--- src/components/SeatsLocationPrice.tsx
+++ src/components/SeatsLocationPrice.tsx
@@ -3,64 +3,27 @@ type SeatsLocationPriceProps = {
   Location: string;
   Price: number;
 };
-type Input = "Front" | "Middle" | "Back";
-const Object: Record<Input, number> = {
-  Front: 12.95,
-  Middle: 14,
-  Back: 16,
+type Seats = "Front" | "Middle" | "Back";
+type InputParams = {
+  type: Seats;
+  number: number;
 };
-// export function SeatsLocationPrice({ [{Front:2}, {Middle:5}, {Back"6}] }: Input) {
-//   return (
-//     {Object.Front}
-
-//   );
-// }
-
-export function 
-const SeatsLocationPrice =new Map<string,string |number>([
- ["Seat Front"],$12.95 / each"],
- ["Seat"],["Middle"] , Middle,$12.95 / each"],
-["Seat" ,Back,$12.95 / each"]'
-]);
-//const map1 = new Map<string, string | number>([
-//   ['name', 'Tom'],
-//   ['country', 'Germany'],
-//   ['age', 30],
-// ]);
-
-// ✅ Using forEach
-map1.forEach((value, key) => {
-  console.log(value, key); // 👉️ Tom name, Germany country, 30 age
-});
-
-/*
-    Higher order functions
-    - sort -> reorders elements in an array => reference to the mutated array
-    - map
-*/
-
-function map(
-  arr: number[],
-  callbackFn: (value: number, index: number, array: number[]) => number
-) {
-  let newArray: number[] = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-    const result = callbackFn(element, i, arr);
-    newArray.push(result);
-  }
-
-  return newArray;
+const prices: Record<Seats, number> = {
+  Front: 12.95,
+  Middle: 14.95,
+  Back: 16.95,
+};
+export function SeatsLocationPrice({ type, number }: InputParams) {
+  return (
+    <div className="flex gap-36 justify-center">
+      <div className="flex gap-4">
+        <p className="text-description">{number}</p>
+        <p className="text-description text-white">Seat - {type}</p>
+      </div>
+      <div className="flex gap-1 text-description ">
+        <p className="text-white">${prices[type]}</p>
+        <p className="">/each</p>
+      </div>
+    </div>
+  );
 }
-
-function double(value: number) {
-  return value * 2;
-}
-
-function square(value: number) {
-  return value * value;
-}
-
-console.log(map([1, 2, 3], double));
-console.log(map([1, 2, 3], square));
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,12 @@
+import { SeatsLocationPrice } from "../components/SeatsLocationPrice";
+
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <div>
+        <h1>Home</h1>
+      </div>
+      <SeatsLocationPrice type={"Front"} number={0}></SeatsLocationPrice>
+    </>
   );
 }
