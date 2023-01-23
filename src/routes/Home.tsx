2e15b11ad7ad94f8e45e52b7d6d62f713978fd61
Marhamat <M.berhan@gmx.de>
2023-01-23 14:12:00 +0100

    edited

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,9 @@
-import { SeatsLocationPrice } from "../components/SeatsLocationPrice";
-
 export function Home() {
   return (
     <>
       <div>
         <h1>Home</h1>
       </div>
-      <SeatsLocationPrice type={"Front"} number={0}></SeatsLocationPrice>
     </>
   );
 }
