13951fff0f950643f2d2bf5750ac815d0c9d2c18
William de Vial <william.d.devial@gmail.com>
2023-01-31 09:16:20 +0100

    prisma.user

--- server/models.ts
+++ server/models.ts
@@ -1,3 +1,24 @@
 import { z } from "zod";
 
-export default {};
+const emailTestType = z.object({
+  userEmail: z.string().email(),
+  userPassword: z.string(),
+});
+const indentifierType = z.object({
+  userIdentifier: z.string(),
+});
+const patchUserInfo = z.object({
+  newName: z.optional(z.string()),
+  newPhoto: z.optional(z.string()),
+  newPassword: z.optional(z.string()),
+});
+const patchTicketInfo = z.object({
+  seatIDs: z.array(z.string()),
+  price: z.string(),
+});
+export default {
+  emailTestType,
+  patchUserInfo,
+  indentifierType,
+  patchTicketInfo,
+};
--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import cors from "@fastify/cors";
 import models from "./models";
 
 import { FastifyZod, buildJsonSchemas, register } from "fastify-zod";
+import { ZodError } from "zod";
 
 // Global augmentation, as suggested by
 // https://www.fastify.io/docs/latest/Reference/TypeScript/#creating-a-typescript-fastify-plugin
@@ -15,6 +16,8 @@ declare module "fastify" {
 const prisma = new PrismaClient();
 
 async function init() {
+  console.log("Hello, world");
+
   const { schemas, $ref } = buildJsonSchemas(models);
 
   // Require the framework and instantiate it
@@ -26,6 +29,93 @@ async function init() {
 
   //EndPoints Go here
 
+  fastify.post("/users/", async (request, response) => {
+    try {
+      const { userEmail, userPassword } = models.emailTestType.parse(
+        request.body
+      );
+      const matchingEmails = await prisma.user.findFirst({
+        where: {
+          email: userEmail,
+        },
+      });
+      if (matchingEmails && userPassword == matchingEmails.password) {
+        response.status(202).send("User Exists");
+        return;
+      } else if (matchingEmails) {
+        response.status(406).send("Wrong Password");
+      } else {
+        response.status(201).send("New User Created");
+        await prisma.user.create({
+          data: {
+            email: userEmail,
+            password: userPassword,
+            name: "John Doe",
+            photo:
+              "https://img.freepik.com/free-icon/user_318-804790.jpg?w=2000",
+          },
+        });
+      }
+    } catch (error) {
+      if (error instanceof ZodError) {
+        response.status(422).send(JSON.stringify(error));
+        return;
+      }
+      response.status(500).send("Wierd error");
+    }
+  });
+
+  fastify.patch("/:userIdentifier", async (request, response) => {
+    try {
+      const { userIdentifier } = models.indentifierType.parse(request.params);
+      const { newName, newPassword, newPhoto } = models.patchUserInfo.parse(
+        request.body
+      );
+      const Test = await prisma.user.update({
+        where: {
+          indentifier: userIdentifier,
+        },
+        data: {
+          name: newName,
+          password: newPassword,
+          photo: newPhoto,
+        },
+      });
+    } catch (error) {
+      if (error instanceof ZodError) {
+        response.status(422).send(JSON.stringify(error));
+        return;
+      }
+      response.status(500).send("Wierd error");
+    }
+  });
+
+  fastify.patch("/:userIdentifier/addTickets", async (request, response) => {
+    try {
+      const { userIdentifier } = models.indentifierType.parse(request.params);
+      const { seatIDs, price } = models.patchTicketInfo.parse(request.body);
+      await prisma.ticket.create({
+        data: {
+          userID: userIdentifier,
+          price: price,
+          seats: {
+            connect: seatIDs.map((x) => {
+              return {
+                identifier: x,
+              };
+            }),
+          },
+        },
+      });
+      response.status(202).send("Linked Tickets to User");
+    } catch (error) {
+      if (error instanceof ZodError) {
+        response.status(422).send(JSON.stringify(error));
+        return;
+      }
+      response.status(500).send("Wierd error");
+    }
+  });
   await fastify.listen({ port: 3999, host: "127.0.0.1" });
 }
 try {
